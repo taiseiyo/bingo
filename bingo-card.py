410ecca8c6ba65bf26310efd54b7645f31912663
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-04-16 19:56:44 +0900

    fix

--- bingo-card.py
+++ bingo-card.py
@@ -10,8 +10,7 @@ window_x, window_y = pyautogui.size()
 window, string_x = (int(window_x/2), window_y), window_x/8
 surface = pygame.display.set_mode(window)
 fpsclock = pygame.time.Clock()
-yellow = (255, 255, 133)
-
+pink = (255, 182, 193)
 bingo_string = pygame.font.SysFont(None, 72)
 number_string = pygame.font.SysFont(None, 100)
 
@@ -20,12 +19,13 @@ def generate_num(x, y, number):
     for i in range(1, 6):
         if (i * 100 <= x <= (i + 1) * 100):
             surface.blit(
-                number_string.render(str(number), True, (0, 255, 255)), (x, y))
+                number_string.render(str(number), True, (0, 106, 182)), (x, y))
 
 
 def main():
     swtich, count = True,  1
     while True:
+
         for get in pygame.event.get():
             if(get.type == KEYDOWN):
                 if(get.key == K_SPACE):
@@ -36,21 +36,20 @@ def main():
                     sys.exit()
 
         if(swtich):
-            surface.fill(yellow)
-            index = 0
-            num_list = []
+            surface.fill(pink)
+            index, index_list = 1, []
+            num_list, number2 = [], []
             for i in range(1, 6):
                 for j in range(1, 6):
                     number1 = random.randint(15*(j - 1) + 1, 15*j)
                     if(number1 not in num_list):
-                        num_list.append(number1)
-                        generate_num(20+100*j, 30+100*i, number1)
+                        generate_num(100*j+20, 100*i+30, number1)
                     else:
-                        index = index + 1
-                        number2 = range(15*(j-1), 15*j)
+                        number2 = range(15*(j-1)+1, 15*j)
                         number2 = list(set(number2) - set(num_list))
-                        generate_num(20+100*j, 30+100*i, number2[index])
-
+                        generate_num(100*j+20, 100*i+30, number2[index])
+                        index = index + 1
+                    num_list.append(number1)
         for i in range(1, 7):
             pygame.draw.line(surface, (255, 0, 0),
                              (100*i, 100), (100*i, 600), 5)
@@ -58,7 +57,7 @@ def main():
                              (100, 100*i), (600, 100*i), 5)
 
         surface.blit(bingo_string.render("BINGO CARD", True,
-                                         (0, 255, 255)), (string_x, 50))
+                                         (0, 128, 0)), (string_x, 50))
 
         pygame.draw.circle(surface, (255, 0, 0), (350, 350), 50)
         fpsclock.tick(40)
