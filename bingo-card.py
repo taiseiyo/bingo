455a8afcc6287d79681bc674cd4bb48f74f68b9a
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-04-17 10:55:37 +0900

    fix

--- bingo-card.py
+++ bingo-card.py
@@ -15,53 +15,60 @@ bingo_string = pygame.font.SysFont(None, 72)
 number_string = pygame.font.SysFont(None, 100)
 
 
-def generate_num(x, y, number):
-    for i in range(1, 6):
-        if (i * 100 <= x <= (i + 1) * 100):
-            surface.blit(
-                number_string.render(str(number), True, (0, 106, 182)), (x, y))
+class Bingo_Card(object):
 
+    def __init__(self):
+        self.switch, self.count = True,  1
+        self.mainloope()
 
-def main():
-    swtich, count = True,  1
-    while True:
+    def generate_num(self, x, y, number):
+        for i in range(1, 6):
+            if (i * 100 <= x <= (i + 1) * 100):
+                surface.blit(
+                    number_string.render(str(number), True, (0, 106, 182)), (x, y))
 
-        for get in pygame.event.get():
-            if(get.type == KEYDOWN):
-                if(get.key == K_SPACE):
-                    swtich = False if count % 2 == 1 else True
-                    count = count + 1 if count < 10 else 1
-                elif(get.key == K_ESCAPE):
-                    pygame.quit()
-                    sys.exit()
-
-        if(swtich):
-            surface.fill(pink)
-            index, index_list = 1, []
-            num_list, number2 = [], []
-            for i in range(1, 6):
-                for j in range(1, 6):
-                    number1 = random.randint(15*(j - 1) + 1, 15*j)
-                    if(number1 not in num_list):
-                        generate_num(100*j+20, 100*i+30, number1)
-                    else:
-                        number2 = range(15*(j-1)+1, 15*j)
-                        number2 = list(set(number2) - set(num_list))
-                        generate_num(100*j+20, 100*i+30, number2[index])
-                        index = index + 1
-                    num_list.append(number1)
+    def draw_normal_object(self):
         for i in range(1, 7):
             pygame.draw.line(surface, (255, 0, 0),
                              (100*i, 100), (100*i, 600), 5)
             pygame.draw.line(surface, (255, 0, 0),
                              (100, 100*i), (600, 100*i), 5)
-
         surface.blit(bingo_string.render("BINGO CARD", True,
                                          (0, 128, 0)), (string_x, 50))
-
         pygame.draw.circle(surface, (255, 0, 0), (350, 350), 50)
-        fpsclock.tick(40)
-        pygame.display.update()
+
+    def mainloope(self):
+        while True:
+            for get in pygame.event.get():
+                if(get.type == KEYDOWN):
+                    if(get.key == K_SPACE):
+                        self.switch = False if self.count % 2 == 1 else True
+                        self.count = self.count + 1 if self.count < 10 else 1
+                    elif(get.key == K_ESCAPE):
+                        pygame.quit()
+                        sys.exit()
+
+            if(self.switch):
+                surface.fill(pink)
+                index = 1
+                num_list, number2 = [], []
+
+                for i in range(1, 6):
+                    for j in range(1, 6):
+                        number1 = random.randint(15*(j - 1) + 1, 15*j)
+                        if(number1 not in num_list):
+                            self.generate_num(100*j+20, 100*i+30, number1)
+                        else:
+                            number2 = range(15*(j-1)+1, 15*j)
+                            number2 = list(set(number2) - set(num_list))
+                            self.generate_num(
+                                100*j+20, 100*i+30, number2[index])
+                            index = index + 1
+                        num_list.append(number1)
+
+            self.draw_normal_object()
+            fpsclock.tick(40)
+            pygame.display.update()
 
 
-main()
+Bingo_Card()
